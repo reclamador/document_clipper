03711daf28d69eb9f89063aecb33af97a3d3989f
Nicolas Jaremek <nick13jaremek@gmail.com>
2017-06-27 11:29:50 +0200

    Added working code for PDF document's text coordinates extraction logic, with tests.

---
+++ document_clipper/pdf.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import re
+from scraperwiki import pdftoxml
+from bs4 import BeautifulSoup
+
+PAGE_TAG_NAME = u'page'
+BOLD_TAG_NAME = u'b'
+TEXT_TAG_NAME = u'text'
+
+
+class DocumentClipperPdf:
+    def __init__(self, pdf_file):
+        self.pdf_file = pdf_file
+        self._pdf_to_xml = None
+
+    def _read_file(self):
+        self.pdf_file.seek(0)
+        return self.pdf_file.read()
+
+    def _str_list_to_int_list(self, str_list):
+        return (float(param) for param in str_list)
+
+    def pdf_to_xml(self):
+        """
+        Returns the XML counterpart of the input PDF file contents in a structure suitable for programmatic manipulation.
+        @param pdf_contents: the contents of a PDF file object
+        @return: a BeautifulSoup structure representing the PDF contents in XML nodes
+        """
+        pdf_file_contents = self._read_file()
+        pdf_contents_to_xml = pdftoxml(pdf_file_contents)
+        self._pdf_to_xml = BeautifulSoup(pdf_contents_to_xml, 'xml')
+        return self._pdf_to_xml
+
+    def get_pages(self):
+        """Returns a list of `page` nodes of the XML contents of a PDF file."""
+        return self._pdf_to_xml.findAll(PAGE_TAG_NAME)
+
+    def find_text_with_content(self, pages, text_to_find, start_page=0):
+        """
+        Given a list of BeautifulSoup 'page' nodes, performs a search of 'text_to_find' on the 'text' nodes of the 'page'
+        nodes. If no text is found, returns None
+        @param pages: a list of BeautifulSoup nodes with 'page' name
+        @param text_to_find: the text to lookup in the 'pages' list in Unicode format
+        @param start_page: optionally specify the starting position from which the 'pages' list should be iterated over.
+        (default=0)
+        @return: the node of type 'text' that contains the searched text, None if no match was found.
+        """
+        looked_up_pages = pages[slice(start_page, None)]  # From some start position to last item (inclusive)
+        content = None
+
+        for page in looked_up_pages:
+            content = page.find(text=re.compile(text_to_find))
+            if content:
+                while content.name != TEXT_TAG_NAME:
+                    content = content.parent
+                break
+
+        return content
+
+    def get_text_coordinates(self, text_node):
+        """
+        Given a BeautifulSoup node of type 'text', fetch the dimensions and location of the node in the PDF file.
+
+        @param text_node: the BeautifulSoup node with name 'text' that should contain attributes with the text's dimensions
+        and location coordinates
+        @return: a list with the location coordinates (left, top) and the dimensions (width, height) of the text associated
+        to the input node.
+        """
+        if text_node.name != TEXT_TAG_NAME:
+            raise Exception(u"Input node is not of type 'text'")
+
+        text_node_attrs = text_node.attrs
+        x_position = text_node_attrs.get('left', '0')
+        y_position = text_node_attrs.get('top', '0')
+        width = text_node_attrs.get('width', '0')
+        height = text_node_attrs.get('height', '0')
+
+        params = [x_position, y_position, width, height]
+
+        # Return dimensions and coordinates as integers instead of strings
+        return self._str_list_to_int_list(params)
+
+    def get_page_max_dimensions(self, page_tag_node):
+        max_page_width = page_tag_node.attrs.get('width', '0')
+        max_page_height = page_tag_node.attrs.get('height', '0')
+        params = [max_page_width, max_page_height]
+
+        return self._str_list_to_int_list(params)
---
+++ tests/test_document_clipper_pdf.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+import os
+from document_clipper.pdf import DocumentClipperPdf
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PATH_TO_PDF_FILE = os.path.join(CURRENT_DIR, 'sample-files/sample.pdf')
+
+
+class TestDocumentClipperPdf(TestCase):
+    def setUp(self):
+        self.pdf_file = open(PATH_TO_PDF_FILE)
+        self.document_clipper_pdf = DocumentClipperPdf(self.pdf_file)
+
+    def tearDown(self):
+        self.pdf_file.close()
+
+    def test_pdf_to_xml_ok(self):
+        pdf_to_xml = self.document_clipper_pdf.pdf_to_xml()
+
+        self.assertTrue(pdf_to_xml.is_xml)
+        self.assertIsNotNone(pdf_to_xml.contents)
+
+    def test_get_pages_ok(self):
+        self.document_clipper_pdf.pdf_to_xml()
+        pages = self.document_clipper_pdf.get_pages()
+        self.assertEqual(len(pages), 10)
+
+    def test_find_text_with_content_ok(self):
+        self.document_clipper_pdf.pdf_to_xml()
+        pages = self.document_clipper_pdf.get_pages()
+        text_node = self.document_clipper_pdf.find_text_with_content(pages=pages,
+                                                   text_to_find=u'chapter 3',
+                                                   start_page=0)
+        self.assertIsNotNone(text_node)
+
+    def test_find_text_with_content_not_ok(self):
+        self.document_clipper_pdf.pdf_to_xml()
+        pages = self.document_clipper_pdf.get_pages()
+        start_page = len(pages) - 1
+        text_node = self.document_clipper_pdf.find_text_with_content(pages=pages,
+                                                                     text_to_find=u'chapter 3',
+                                                                     start_page=start_page)
+        self.assertIsNone(text_node)
+
+    def test_get_text_coordinates_ok(self):
+        EXPECTED_X_POSITION = 149.0
+        EXPECTED_Y_POSITION = 493.0
+        EXPECTED_WIDTH = 350.0
+        EXPECTED_HEIGHT = 16.0
+
+        self.document_clipper_pdf.pdf_to_xml()
+        pages = self.document_clipper_pdf.get_pages()
+        text_node = self.document_clipper_pdf.find_text_with_content(pages=pages,
+                                                                     text_to_find=u'chapter 3',
+                                                                     start_page=0)
+        x_position, y_position, width, height = self.document_clipper_pdf.get_text_coordinates(text_node)
+
+        self.assertEqual(x_position, EXPECTED_X_POSITION)
+        self.assertEqual(y_position, EXPECTED_Y_POSITION)
+        self.assertEqual(width, EXPECTED_WIDTH)
+        self.assertEqual(height, EXPECTED_HEIGHT)
+
+    def test_get_page_dimensions_ok(self):
+        EXPECTED_MAX_PAGE_WIDTH = 892.0
+        EXPECTED_MAX_PAGE_HEIGHT = 1262.0
+
+        self.document_clipper_pdf.pdf_to_xml()
+        pages = self.document_clipper_pdf.get_pages()
+        first_page = pages[0]
+
+        page_width, page_height = self.document_clipper_pdf.get_page_max_dimensions(first_page)
+
+
+        self.assertEqual(page_width, EXPECTED_MAX_PAGE_WIDTH)
+        self.assertEqual(page_height, EXPECTED_MAX_PAGE_HEIGHT)
