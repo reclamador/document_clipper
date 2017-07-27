5ccd3fa3d6419e0e81dde23c4fb65deb1e2c3e0b
Juan Madurga <jlmadurga@gmail.com>
2017-07-27 15:26:09 +0200

    add DocumentClipperPdfWriter

--- document_clipper/pdf.py
+++ document_clipper/pdf.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 import re
+import logging
+import os
+import imghdr
 from scraperwiki import pdftoxml
 from bs4 import BeautifulSoup
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from pilkit.processors import ResizeToFit
+from PIL import Image
+from tempfile import NamedTemporaryFile
+
 
 PAGE_TAG_NAME = u'page'
 BOLD_TAG_NAME = u'b'
 TEXT_TAG_NAME = u'text'
 
 
-class DocumentClipperPdf:
+class DocumentClipperError(Exception):
+    pass
+
+
+class DocumentClipperPdfReader:
     def __init__(self, pdf_file):
         self.pdf_file = pdf_file
         self._pdf_to_xml = None
@@ -100,3 +112,102 @@ class DocumentClipperPdf:
         params = [max_page_width, max_page_height]
 
         return self._str_list_to_int_list(params)
+
+
+class DocumentClipperPdfWriter:
+
+    MAX_SIZE_WITH_MARGINS = (2400, 3400)
+    MAX_SIZE_IN_PIXELS = (2480, 3508)
+    MARGIN_LEFT = 40
+    MARGIN_TOP = 54
+
+
+    def __init__(self, max_size_in_pixels=MAX_SIZE_IN_PIXELS, max_size_with_margins=MAX_SIZE_WITH_MARGINS,
+                 margin_left=MARGIN_LEFT, margin_top=MARGIN_TOP):
+        self.max_size_with_margins = max_size_with_margins
+        self.max_size_in_pixels = max_size_in_pixels
+        self.margin_left = margin_left
+        self.margin_top = margin_top
+
+    def image_to_pdf(self, img, pdf_path=None, **kwargs):
+        """
+        Convert image to pdf.
+        :param img: image file opened by PIL
+        :param pdf_path: path to save pdf
+        :param kwargs: any parameter accepted by Image.save i.e. quality
+        :return:
+        """
+
+        processor = ResizeToFit(self.MAX_SIZE_WITH_MARGINS[0], self.MAX_SIZE_WITH_MARGINS[1])
+        img = processor.process(img)
+        # Create a white canvas and paste the image
+        tmp_image = Image.new("RGB", self.max_size_in_pixels, "white")
+        tmp_image.paste(img, (self.margin_left, self.margin_top,
+                        self.margin_left + img.size[0], self.margin_top + img.size[1]))
+
+        # Save the image as .pdf file
+        if not pdf_path:
+            f = NamedTemporaryFile(delete=False)
+            pdf_path = f.name
+        tmp_image.save(pdf_path, "PDF", resolution=100.0, **kwargs)
+
+        return pdf_path
+
+    def merge_pdfs(self, final_pdf_path, pdf_files_paths, blank_page=True):
+        """
+        Merge pdf files in only one PDF
+        :param final_pdf_path: file path to save pdf
+        :param pdf_files_paths: PDF file paths to merge
+        :param blank_page: True if want a blank page between pdf
+        :return:
+        """
+
+        """ Merge all pdf of a folder in one single file '.pdf'. """
+
+        output = PdfFileWriter()
+
+        for num_doc, pdf_file in enumerate(pdf_files_paths):
+            if pdf_file == final_pdf_path:
+                continue
+
+            if not pdf_file:
+                continue
+
+            logging.info(u"Parse '%s'" % pdf_file)
+
+            try:
+                document = PdfFileReader(open(pdf_file, 'rb'), strict=False)
+                num_pages = document.getNumPages()
+            except Exception as exc:
+                logging.exception("Error merging pdf %s: %s" % (pdf_file, str(exc)))
+                raise DocumentClipperError
+
+            for num_page in range(num_pages):
+                output.addPage(document.getPage(num_page))
+
+            if blank_page:
+                output.addBlankPage()
+
+        logging.info(u"Start writing '%s'" % final_pdf_path)
+        output_stream = file(final_pdf_path, "wb")
+        output.write(output_stream)
+        output_stream.close()
+
+    def merge(self, final_pdf_path, files_paths, blank_page=False):
+        """
+        Merge files (images and pdfs) in to one PDF
+        :param final_pdf_path: file path to save pdf
+        :param files_paths: file paths to merge
+        :param blank_page: True if want a blank page between pdf
+        :return:
+        """
+        real_file_paths = []
+        for file_path in files_paths:
+            if imghdr.what(file_path):
+                img = Image.open(file_path)
+                real_file_paths.append(self.image_to_pdf(img))
+            else:
+                extension = os.path.splitext(file_path)[1]
+                if extension.lower() == ".pdf":
+                    real_file_paths.append(file_path)
+        self.merge_pdfs(final_pdf_path, real_file_paths, blank_page)
